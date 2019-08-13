39417c0146edf5b2078eefd90458313167d05c0a
Keigh Rim <keigh.rim@gmail.com>
2019-08-12 22:15:31 -0400

    (MAC) made changes for k810

--- mac/k810fn.cpp
+++ mac/k810fn.cpp
@@ -14,35 +14,38 @@ int main(int argc, char* argv[])
 
 	static const int LOGITECH = 0x046d;
 	static const int K811 = 0xb317;
+	static const int K810 = 0xb319;
+    static const char help[64] = "Usage: k810fn <-normal|-special>\n";
 
 	if (argc != 2) {
-		printf("Usage: k811fn <-normal|-special>\n");
+		printf(help);
 		return 1;
 	}
 
 	// found using Packet Logger
 	memset(buf,0,sizeof(buf));
-	buf[0] = 0x10;
-	buf[1] = 0xFF;
-	buf[2] = 0x06;
-	buf[3] = 0x14;
+	buf[0] = 0x0;
+	buf[1] = 0x10;
+	buf[2] = 0xFF;
+	buf[3] = 0x06;
+	buf[4] = 0x15;
 	// 5 is set below
-	buf[5] = 0x00;
 	buf[6] = 0x00;
+	buf[7] = 0x00;
 
 	if (strcmp(argv[1], "-normal") == 0) {
-		buf[4] = 0x00; // f#-keys
+		buf[5] = 0x00; // f#-keys
 	} else if (strcmp(argv[1], "-special") == 0) {
-		buf[4] = 0x01; // function keys
+		buf[5] = 0x01; // function keys
 	} else {
-		printf("Usage: k811fn <-normal|-special>\n");
+		printf(help);
 		return 1;
 	}
 
 	res = hid_init();
 
 	printf("Discovering devices ...\n");
-	devs = hid_enumerate(LOGITECH, K811);
+	devs = hid_enumerate(LOGITECH, K810);
 	cur_dev = devs;
 	while (cur_dev) {
 		if (cur_dev->usage == 6) {
